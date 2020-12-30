474a0326fb1194aa1bf98d0520071ad20e2c9784
jimmywilson111 <jimmy.wilson11@gmail.com>
2020-12-30 17:12:12 +0200

    #2: Add styles and ETC

---
+++ plugins/NewTheme/src/Resources/app/storefront/src/main.js
@@ -0,0 +1,5 @@
+import QuantityFieldPlugin from "./script/quantity-field.plugin"
+import newsTicker from "./script/inewsticker";
+
+window.PluginManager.register('QuantityFieldPlugin', QuantityFieldPlugin, '[data-quantity-field]')
+
